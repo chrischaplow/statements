5eb1683ca8f0e9a89df33f6c913e5d080c1c31f0
chrischaplow <chris@chaplow.com>
2019-08-25 18:53:31 +0200

    adding a second

---
+++ ifStatements.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Title</title>
+    </head>
+    <body>
+
+<?php 
+$a=100; $b=200;
+if ($a>$b){
+echo "a is greater than b.";
+}
+else {
+echo "a is less than b";
+}
+?>
+
+    </body>
+    </html>
+
+
+        
+
---
+++ ifStatements_1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Title</title>
+    </head>
+    <body>
+
+<?php 
+$a=300; $b=200;
+if ($a>$b){
+echo "a is greater than b.";
+}
+else {
+echo "a is less than b";
+}
+?>
+
+    </body>
+    </html>
+
+
+        
+
